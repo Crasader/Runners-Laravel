bf68ec01d43ea283e6aff448235e35a9f84ef56d
Bastien <bastien.nicoud@gmail.com>
2018-02-05 08:43:27 +0100

    Mix config update

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ let mix = require('laravel-mix');
 mix.disableNotifications();
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+  .sass('resources/assets/sass/app.scss', 'public/css')

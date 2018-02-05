3aeb0d956acfb4ef6142ba87493f22043fbd7fe5
Unknown <nicolas.henry@cpnv.ch>
2018-02-05 09:59:27 +0100

    Add footer blade and change welcome

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Runners</strong> by <a href="#">Bastien Nicoud</a> and<a href="#"> Nicolas Henry</a>
+                ©CPNV - {{date("Y")}}
+            </p>
+        </div>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Runners</h1>
+    <div class="container">
+        <div class="columns">
+            <div class="column is-half">
+                Content 1
+            </div>
+            
+            <div class="column is-half">
+                Content 2
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column is-half">
+                Content 3
+            </div>
+            
+            <div class="column is-half">
+                Content 4
+            </div>
+        </div>
+        
+    </div>
 @endsection

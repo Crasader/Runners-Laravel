5c7804d652a3866f423a7a93a18d67c4b7a2b908
Nico <nicolas.henry@cpnv.ch>
2018-02-09 15:07:13 +0100

    Add menu on navbar, bg-color on h2 and javascript for menu

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -13,38 +13,42 @@
 
         <div class="navbar-menu">
             <div class="navbar-start">
-                <a class="navbar-item" href="https://bulma.io/">
-                    Home
-                </a>
                 <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link" href="/documentation/overview/start/">
-                        Docs
+                    <a class="navbar-link" href="/courses">
+                        Courses
                     </a>
                     <div class="navbar-dropdown is-boxed">
-                        <a class="navbar-item" href="/documentation/overview/start/">
-                            Overview
-                        </a>
-                        <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
-                            Modifiers
-                        </a>
-                        <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                            Columns
-                        </a>
-                        <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                            Layout
-                        </a>
-                        <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                            Form
-                        </a>
-                        <hr class="navbar-divider">
-                        <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                            Elements
-                        </a>
-                        <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                            Components
+                        <a class="navbar-item" href="/courses/create">
+                            Créer courses
                         </a>
                     </div>
                 </div>
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link" href="/voitures">
+                        Voitures
+                    </a>
+                    <div class="navbar-dropdown is-boxed">
+                        <a class="navbar-item" href="/voitures/create">
+                            Créer voiture
+                        </a>
+                    </div>
+                </div>
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link" href="/chauffeurs">
+                        Chauffeurs
+                    </a>
+                    <div class="navbar-dropdown is-boxed">
+                        <a class="navbar-item" href="/chauffeurs/create">
+                            Créer chauffeurs
+                        </a>
+                    </div>
+                </div>
+                <a class="navbar-item" href="/groupes">
+                    Groupes
+                </a>
+                <a class="navbar-item" href="/horaires">
+                    Horaires
+                </a>
             </div>
 
             <div class="navbar-end">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,22 +4,29 @@
     <div class="container">
         <div class="columns">
             <div class="column is-half">
-                Runs
+                <div class="notification is-warning">
+                    <h2 class="title is-2 has-text-centered"><a href="#">Runs</a></h2>
+                </div>
             </div>
             
             <div class="column is-half">
-                Chauffeurs
+                <div class="notification is-primary">
+                    <h2 class="title is-2 has-text-centered"><a href="#">Chauffeurs</a></h2>
+                </div>
             </div>
         </div>
         <div class="columns">
             <div class="column is-half">
-                Véhicules
+                <div class="notification is-success">
+                    <h2 class="title is-2 has-text-centered"><a href="#">Véhicules</a></h2>
+                </div>
             </div>
             
             <div class="column is-half">
-                Stats
+                <div class="notification is-info">
+                    <h2 class="title is-2 has-text-centered"><a href="#">Stats</a></h2>
+                </div>
             </div>
         </div>
-        
     </div>
 @endsection

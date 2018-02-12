61de8dfbeeab7f12cea9d6fb3ae117d40e451ef0
Bastien <bastien.nicoud@gmail.com>
2018-02-12 20:13:07 +0100

    Add pdf export of the MLD

--- database/migrations/2018_02_08_172120_add_group_user_table.php
+++ database/migrations/2018_02_08_172120_add_group_user_table.php
@@ -14,7 +14,6 @@ class AddGroupUserTable extends Migration
     public function up()
     {
         Schema::create('group_user', function (Blueprint $table) {
-            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->integer('group_id')->unsigned();
             $table->timestamps();

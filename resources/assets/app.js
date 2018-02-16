fc4c16a3dc66066ac9d012e2a6cc7f1e80cfe93d
Bastien <bastien.nicoud@gmail.com>
2018-02-16 10:58:49 +0100

    Scaffold the client side app

---
+++ resources/assets/app.js
@@ -0,0 +1,40 @@
+/*
+ |--------------------------------------------------------------------------
+ | Client side app entry point
+ |--------------------------------------------------------------------------
+ |
+ | This file initialise the client-side app
+ | You foud here all the imports and library instanciations
+ | All the assets are compiled/transpiled by webpack to generate dist files in the public directory
+ |
+ */
+
+// Vue main
+import Vue from 'vue'
+import App from './App'
+
+// Axios (Xhttp calls)
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// Vue modules (client-side routing and state managment)
+import router from './router'
+import store from './store'
+
+// Css framework
+import Buefy from 'buefy'
+import './globals.scss'
+
+// Vue initialisation
+Vue.use(Buefy)
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false
+
+// Initialize the vue app
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,4 @@ let mix = require('laravel-mix')
 
 mix.disableNotifications()
 
-mix.js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css')
+mix.js('resources/assets/app.js', 'public/js')

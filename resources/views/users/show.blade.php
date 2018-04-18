1f946a31fd09b448c49dccecc4ae3063da96d15f
Bastien <bastien.nicoud@gmail.com>
2018-04-18 17:13:24 +0200

    Update the show user page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,7 +16,47 @@
 <div class="section">
     <div class="container">
         <div class="columns">
+            <div class="column is-8">
+                <h1 class="title is-2">{{ $user->name }}</h1>
+            </div>
+            <div class="column is-4">
+                @can('create', App\User::class)
+                    <a href="{{ route('users.create') }}" class="button is-info is-pulled-right">Nouvel utilisateur</a>
+                @endcan
+            </div>
+        </div>
 
+        <div class="columns">
+            <div class="column is-4">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image">
+                            <img src="{{ asset(Storage::url($user->profilePictures->first()->path)) }}" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">John Smith</p>
+                                <p class="subtitle is-6">@johnsmith</p>
+                            </div>
+                        </div>
+                    
+                        <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                            <a href="#">#css</a> <a href="#">#responsive</a>
+                            <br>
+                            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>

d512898e41e0ef2de848e7590aee7485dbf7a6df
Bastien <bastien.nicoud@gmail.com>
2018-05-22 16:41:57 +0200

    Add validation rules for new run

--- app/Http/Requests/Runs/StoreNewRun.php
+++ app/Http/Requests/Runs/StoreNewRun.php
@@ -31,7 +31,14 @@ class StoreNewRun extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'                   => ['required', 'string', 'min:1', 'max:200'],
+            'artist'                 => ['required', 'string', 'min:1', 'max:200'],
+            'planned_at'             => ['nullable', 'date'],
+            'end_planned_at'         => ['nullable', 'date'],
+            'waypoint.*'             => ['sometimes', 'string'],
+            'subscription.*.user'    => ['sometimes', 'string', 'exists:users,name'],
+            'subscription.*.carType' => ['sometimes', 'string', 'exists:car_types,name'],
+            'subscription.*.car'     => ['sometimes', 'string', 'exists:cars,name']
         ];
     }
 }

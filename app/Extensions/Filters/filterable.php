5e44d99fd19d48f0ecaa32a3874922e790f8e7c7
Bastien <bastien.nicoud@gmail.com>
2018-05-30 17:15:53 +0200

    Ass between filter

--- app/Extensions/Filters/filterable.php
+++ app/Extensions/Filters/filterable.php
@@ -21,6 +21,13 @@ trait Filterable
      */
     public function scopeFilter($query, $request)
     {
+        if ($request->query('filter')) {
+            $filters = collect($request->query('filters'));
+            $filters->each(function ($filter) {
+                //
+            });
+            //return $query->where($filter);
+        }
         return $query;
     }
 
@@ -33,6 +40,24 @@ trait Filterable
      */
     public function scopeFilterOrder($query, $request)
     {
+        if ($request->query('orderby')) {
+            return $query;
+        }
+        return $query;
+    }
+
+    /**
+     * scopeFilterBetween
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeFilterBetween($query, $request)
+    {
+        if ($request->query('between')) {
+            return $query;
+        }
         return $query;
     }
 }

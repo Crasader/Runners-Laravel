0d1c23835169a102c08f792dce5d5a99a66f25d2
Bastien <bastien.nicoud@gmail.com>
2018-06-05 20:29:41 +0200

    Add logs table

---
+++ database/migrations/2018_06_05_202656_add_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('loggable_id')->unsigned()->nullable();
+            $table->string('loggable_type')->nullable();
+            $table->string('action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop the table
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('logs');
+        Schema::enableForeignKeyConstraints();
+    }
+}

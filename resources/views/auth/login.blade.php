ef585dd65a2ab44dce587472b143369aa73e3f0c
Bastien <bastien.nicoud@gmail.com>
2018-06-06 08:07:09 +0200

    Little changes on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,60 +4,86 @@
 <div class="section">
     <div class="columns">
         <div class="column panel panel-default is-6 is-offset-3">
-            <div class="title is-1">Login</div>
-            <hr>
 
             <div class="panel-body">
                 <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                     {{ csrf_field() }}
 
-                    <div class="field form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class=" control-label label">E-Mail Address</label>
-
-                        <div class="control">
-                            <input id="email" type="email" class="form-control input is-info" name="email" value="{{ old('email') }}" required autofocus>
-
-                            @if ($errors->has('email'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
+                    {{-- TITLE --}}
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label"></label>
+                        </div>
+                        <div class="field-body">
+                            <h1 class="title is-2">Login</h1>
                         </div>
                     </div>
 
-                    <div class="field form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password" class="control-label label">Password</label>
-
-                        <div class="">
-                            <input id="password" type="password" class="form-control input is-info" name="password" required>
-
-                            @if ($errors->has('password'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                            @endif
+                    {{-- EMAIL --}}
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label">Email</label>
+                        </div>
+                        <div class="field-body">
+                            {{-- LASTNAME --}}
+                            @component('components/horizontal_form_input', [
+                                'name'        => 'email',
+                                'placeholder' => "Votre email utilisé pour s'authentifier",
+                                'type'        => 'text',
+                                'icon'        => 'fa-user',
+                                'errors'      => $errors
+                                ])
+                            @endcomponent
                         </div>
                     </div>
 
-                    <div class="field form-group">
-                        <div class="">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                </label>
+                    {{-- PASSWORD --}}
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label">Email</label>
+                        </div>
+                        <div class="field-body">
+                            {{-- LASTNAME --}}
+                            @component('components/horizontal_form_input', [
+                                'name'        => 'password',
+                                'placeholder' => "Votre mot-de-passe",
+                                'type'        => 'password',
+                                'icon'        => 'fa-lock',
+                                'errors'      => $errors
+                                ])
+                            @endcomponent
+                        </div>
+                    </div>
+
+                    {{-- REMEMBER ME --}}
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label"></label>
+                        </div>
+                        <div class="field-body">
+                            <div class="field is-narrow">
+                                <div class="control">
+                                    <label class="checkbox">
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                    </label>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="field form-group">
-                        <div class="control">
-                            <button type="submit" class="btn btn-primary button is-info">
-                                Login
-                            </button>
-
-                            <a class="btn btn-link button is-info" href="{{ route('password.request') }}">
-                                Forgot Your Password?
-                            </a>
+                    {{-- BUTTONS --}}
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label"></label>
+                        </div>
+                        <div class="field-body">
+                            <div class="field is-narrow">
+                                <div class="control">
+                                    <button type="submit" class="btn btn-primary button is-info">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </form>

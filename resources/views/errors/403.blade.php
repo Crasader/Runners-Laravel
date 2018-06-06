d10332d6d03fbe3613add5bb84c2c451d8312bb5
Bastien <bastien.nicoud@gmail.com>
2018-06-06 21:03:05 +0200

    Add 403 error

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,33 @@
+{{--
+  -- 403 error page
+  --
+  --}}
+@extends('layouts.app')
+
+@section('breadcrum')
+<li class="is-active"><a href="#" aria-current="page">Erreur 404</a></li>
+@endsection
+
+@section('content')
+
+<div class="section homepage">
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-narrow">
+                <h1 class="title is-1">- 403 -</h1>
+            </div>
+        </div>
+        <div class="columns is-centered">
+            <div class="column is-narrow">
+                <h2 class="title is-3">Accès refusé !</h2>
+            </div>
+        </div>
+        <div class="columns is-centered">
+            <div class="column is-narrow">
+                <h2 class="title is-2">{{ $exception->getMessage() }}</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection

7446d6f2be55db1d3bd6415b1509d826f4713e8d
MisterX25 <Xavier.Carrel@cpnv.ch>
2018-07-20 15:45:39 +0200

    Changed the 'problem' status threshold to -10 minutes

--- app/Run.php
+++ app/Run.php
@@ -491,8 +491,8 @@ class Run extends Model
     {
         $deltatime = (new Carbon())->diffInMinutes(new Carbon($this->planned_at),false);
         if ($this->status == 'error') return true;
-        if ($this->status == 'ready' && $deltatime <= 10) return true;
-        if ($this->status == 'needs_filling' && $deltatime < 10) return true;
+        if ($this->status == 'ready' && $deltatime <= -10) return true;
+        if ($this->status == 'needs_filling' && $deltatime <= -10) return true;
         return false;
     }
 

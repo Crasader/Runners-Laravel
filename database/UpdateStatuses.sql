871fd58baf4737603e5ef6ee52871c9c89681a6c
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-01-22 13:27:21 +0100

    db stuff

--- database/UpdateStatuses.sql
+++ database/UpdateStatuses.sql
@@ -1,2 +1,3 @@
+update runs set planned_at = date_add(planned_at, interval 55 day);
 update runs set status = 'finished' where planned_at < DATE_SUB(NOW(), INTERVAL 1 hour);
 update runs set status = 'gone' where status <> 'finished' and planned_at < DATE_ADD(NOW(), INTERVAL 30 minute);
